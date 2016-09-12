5b2f0ce3952067b3ab1f8075b2a64ef5a37f553d
tasaka-kazunobu <tasanobu@gmail.com>
2016-09-12 16:18:44 +0900

    Add the target of tvOS framework

---
+++ ColorAduster-tvOS/ColorAduster-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  ColorAduster-tvOS.h
+//  ColorAduster-tvOS
+//
+//  Created by Kazunobu Tasaka on 9/12/16.
+//  Copyright © 2016 mai_ikeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ColorAduster-tvOS.
+FOUNDATION_EXPORT double ColorAduster_tvOSVersionNumber;
+
+//! Project version string for ColorAduster-tvOS.
+FOUNDATION_EXPORT const unsigned char ColorAduster_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColorAduster_tvOS/PublicHeader.h>
+
+
